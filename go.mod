7bbbeac9090bf4cfba5a956710383486fb403ccd
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2022-12-17 07:40:19 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/jtbonhomme/homekit-test
+
+go 1.19
+
+require (
+	github.com/brutella/dnssd v1.2.3 // indirect
+	github.com/brutella/hap v0.0.20 // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/miekg/dns v1.1.50 // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
+	github.com/tadglines/go-pkgs v0.0.0-20210623144937-b983b20f54f9 // indirect
+	github.com/xiam/to v0.0.0-20200126224905-d60d31e03561 // indirect
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"time"
+
+	"github.com/brutella/hap"
+	"github.com/brutella/hap/accessory"
+	"github.com/rs/zerolog"
+
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	output := zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+	}
+
+	logger := zerolog.New(output).With().Timestamp().Logger()
+	logger.Info().Msg("Homekit demo starting...")
+
+	// Create the testing accessory.
+	a := accessory.NewSwitch(accessory.Info{
+		Name: "MBP-JTB",
+	})
+	a.Switch.On.OnValueRemoteUpdate(func(on bool) {
+		if on == true {
+			logger.Info().Msg("Switch is on")
+		} else {
+			logger.Info().Msg("Switch is off")
+		}
+	})
+	logger.Info().Msgf("Homekit demo new accessory created: %s", a.Name())
+
+	// Store the data in the "./db" directory.
+	fs := hap.NewFsStore("./db")
+	logger.Info().Msg("Homekit demo db created")
+
+	// Create the hap server.
+	server, err := hap.NewServer(fs, a.A)
+	if err != nil {
+		// stop if an error happens
+		logger.Panic().Msgf("failed while creating new HAP server: %s", err.Error())
+	}
+	logger.Info().Msg("Homekit demo server started")
+
+	// Setup a listener for interrupts and SIGTERM signals
+	// to stop the server.
+	logger.Info().Msg("Waiting for SIGTERM signal ...")
+	// Waiting signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-interrupt
+		logger.Info().Msg("\nReceived SIGTERM signal !\n")
+		// Stop delivering signals.
+		signal.Stop(interrupt)
+		// Cancel the context to stop the server.
+		cancel()
+	}()
+
+	// Run the server.
+	logger.Info().Msg("Listening ...")
+	server.ListenAndServe(ctx)
+	logger.Info().Msg("Exit")
+}
